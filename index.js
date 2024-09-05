051a8cf77d985479bbea344c72c737156a6968be
almas hossain <almashossain7384@gmail.com>
2024-09-05 19:30:00 +0600

    navbar update

---
+++ index.js
@@ -0,0 +1,445 @@
+const express = require('express')
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000
+
+
+
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173',],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+// mongodb connected here....
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1kmrgvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Another collection here....
+    const usersCollection = client.db('BloodQuestDB').collection('users')
+    const donorCollection = client.db('BloodQuestDB').collection('donor')
+    const blogsCollection = client.db('BloodQuestDB').collection('blog')
+    const fundsCollection = client.db('BloodQuestDB').collection('fund')
+
+
+
+    //======================== user related api part start ========================= */
+    app.post('/users/api/create', async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email }
+
+      const isexist = await usersCollection.findOne(query);
+      if (isexist) {
+        return res.json({
+          message: 'User alredy exist'
+        })
+      }
+      else {
+        const result = await usersCollection.insertOne(user)
+        res.send(result)
+      }
+    });
+
+    // ok...
+    app.get('/users/related/api/get', async (req, res) => {
+      const { status } = req.query;
+
+      let filter = {};
+      if (status) {
+        filter = { status };
+      }
+      const result = await usersCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    //ok.......
+    app.get('/user/profile/api/get/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+
+    // ok...
+    app.put('/userProfile/update/api/:email', async (req, res) => {
+      const email = req.params.email;
+      const data = req.body;
+      const filter = { email: email }
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...data
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+    //ok.......
+    app.get('/user/role/api/get/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email: email });
+      res.send(result);
+    });
+
+
+    //======================== user related api part end =========================== */
+
+
+    //======================== donor related api part start ========================= */
+    //ok............
+    app.post('/donor/donation/request/api/create', async (req, res) => {
+      const user = req.body;
+      const result = await donorCollection.insertOne(user)
+      res.send(result)
+    });
+
+    // ok.........
+    app.get('/recent/donation/request/api/get/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { requesterEmail: email }
+
+      const recentData = await donorCollection.find(filter).sort({ donationDate: -1 }).limit(3).toArray();
+      res.send(recentData);
+    })
+
+    // ok.............
+    app.get('/allDonor/donation/request/api/get/:email', async (req, res) => {
+      const email = req.params.email;
+      const filterData = req.query.filterQuery;
+
+      if (email) {
+        const filter = { requesterEmail: email }
+        const result = await donorCollection.find(filter).toArray();
+
+        if (filterData) {
+          const filterResult = result.filter(data => data.status === filterData);
+          res.send(filterResult)
+        }
+        else {
+          res.send(result)
+        }
+      }
+    })
+
+    // ok.........
+    app.get('/donor/donation/request/api/get/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await donorCollection.findOne(query)
+      res.send(result)
+    })
+
+    // ok............
+    app.delete('/donor/donation/request/api/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await donorCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // ok.......
+    app.put('/donor/donation/request/api/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          ...updateData
+        },
+      };
+      const result = await donorCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
+    //======================== donor related api part end =========================== */
+
+
+    //======================== volunteer  related api part start ========================= */
+    // ok...
+    app.get('/allBlood/donation/request/volunteer/api/get', async (req, res) => {
+      const result = await donorCollection.find().toArray();
+      res.send(result)
+    })
+
+
+    //======================== volunteer related api part end ============================= */
+
+
+
+    //======================== admin  related api part start ========================= */
+    // ok......
+    app.patch('/user/status/change/api/:email', async (req, res) => {
+      const email = req.params.email;
+      const status = req.body.status;
+      const filter = { email: email }
+
+      let updateStatus = '';
+      if (status === 'block') {
+        updateStatus = 'blocked'
+      }
+      else if (status === 'unblock') {
+        updateStatus = 'active'
+      }
+
+      const updateDoc = {
+        $set: {
+          status: updateStatus,
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    // ok......
+    app.patch('/user/role/change/api/:email', async (req, res) => {
+      const email = req.params.email;
+      const role = req.body;
+      const filter = { email: email }
+      const updateDoc = {
+        $set: {
+          ...role
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    app.get('/all-donation-request/api/get', async (req, res) => {
+      const result = await donorCollection.find().toArray();
+      res.send(result);
+    })
+
+    // ok....
+    app.get('/blood/donation-request/related/api/get/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await donorCollection.findOne(query)
+      res.send(result)
+    })
+
+    // ok......
+    app.put('/admin/donation/request/api/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          ...updateData
+        },
+      };
+      const result = await donorCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
+
+    // ok.....
+    app.delete('/blood/donation-request/related/api/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await donorCollection.deleteOne(query)
+      res.send(result)
+    })
+    //======================== admin related api part end ============================= */
+
+
+
+    //======================== blog count related api part start ========================= */
+    // ok....
+    app.post('/blog/related/api/create', async (req, res) => {
+      const blogData = req.body;
+      const result = await blogsCollection.insertOne(blogData)
+      res.send(result)
+    })
+
+    // ok.....
+    app.get('/allBlog/related/api/get', async (req, res) => {
+      const status = req.query.sort
+
+      let filter = {}
+      if (status === 'draft') {
+        filter.status = status
+      }
+      else if (status === 'published') {
+        filter.status = status
+      }
+
+      const result = await blogsCollection.find(filter).toArray();
+      res.send(result)
+    })
+    // ok...
+    app.get('/blogData/related/api/get/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await blogsCollection.findOne(query)
+      res.send(result)
+    })
+
+    //ok....
+    app.delete('/blog/releted/api/delete/:id', async (req, res) => {
+      const deleteId = req.params.id
+      const query = { _id: new ObjectId(deleteId) }
+      const result = await blogsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // ok...
+    app.put('/blog/related/api/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          ...updateData
+        },
+      };
+      const result = await blogsCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
+
+    // ok...
+    app.patch('/blog/related/api/update/status/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body.status;
+      const query = { _id: new ObjectId(id) }
+
+      let updateDoc = {};
+      // Check if the status is 'draft'
+      if (updateData === 'published') {
+        updateDoc = {
+          $set: {
+            status: 'draft',
+          },
+        };
+      }
+      // Check if the status is 'published'
+      else if (updateData === 'draft') {
+        updateDoc = {
+          $set: {
+            status: 'published'
+          }
+        }
+      }
+
+      const result = await blogsCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+    //======================== blog related api part end ============================= */
+
+
+    //======================== common api part start ========================= */
+    // ok....
+    app.patch('/status/change/related/api/:id', async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const filter = { _id: new ObjectId(id) }
+
+      let updateStatus = status;
+
+
+      const updateDoc = {
+        $set: {
+          status: updateStatus,
+        },
+      };
+
+      const result = await donorCollection.updateOne(filter, updateDoc);
+      res.send(result)
+
+    })
+    //======================== common api part end ============================= */
+
+
+    //======================== navbar route api part start ========================= */
+    //ok..
+    app.get('/navbar/blood/donation/request/pending/api/get', async (req, res) => {
+      const result = await donorCollection.find({ status: 'pending' }).toArray();
+      res.send(result)
+    })
+
+    // ok....
+    app.get('/navbar/viewDetails/blood/request/api/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await donorCollection.findOne(query)
+      res.send(result)
+    })
+
+    //  ok...
+    app.get('/navbar/user/bloodGroup/related/api/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { requesterEmail: email }
+      const result = await donorCollection.findOne(query)
+      res.send(result)
+    })
+
+    // ok...
+    app.get('/navbar/published/blog/related/api/get',async(req, res)=>{
+      const result = await blogsCollection.find({status : 'published'}).toArray();
+      res.send(result)
+    })
+
+    // ok...
+    app.get('/navbar/publishedDetails/related/api/get/:id', async (req, res)=>{
+      const id= req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await blogsCollection.findOne(query)
+      res.send(result)
+    })
+    //======================== navbar route api part end ============================= */
+
+
+    //======================== donor count related api part start ========================= */
+    // ok.........
+    app.get('/admin/donor/count/related/api/get', async (req, res) => {
+      const query = { role: 'donor' };
+      const total_user = await usersCollection.countDocuments(query);
+
+      const total_blood_donation_request = await donorCollection.estimatedDocumentCount();
+      res.send({ total_user, total_blood_donation_request })
+    });
+    //======================== donor count related api part end =========================== */
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('blood quest server')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
